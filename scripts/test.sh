334bee9dd33eb196bd131afae6d4ae6764f3d23a
sheep <funk.ml.ops@proton.me>
2024-02-12 11:53:41 -0600

    cleaning up bad files

--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-PARENT_DIR=$(dirname "$SCRIPT_DIR")
-DIR_NAME=$(basename "$SCRIPT_DIR")
-echo $DIR_NAME
-echo $SCRIPT_DIR
-echo $PARENT_DIR
